1f9a6030af91e37ae5c3177c96bd776ea68b61cd
linshaokui@foxmail.com <627324950@qq.com>
2017-12-27 23:16:50 +0800

    修改设置

--- src/main/java/com/follower/fizz/controller/SendMessageInterceptor.java
+++ src/main/java/com/follower/fizz/controller/SendMessageInterceptor.java
@@ -1,8 +1,12 @@
 package com.follower.fizz.controller;
 
+import com.follower.fizz.entity.AuthStatusMessage;
+import com.follower.fizz.entity.SendMessageStatus;
 import com.follower.fizz.entity.SessionParam;
 import com.follower.fizz.service.ICheckSendMessageAuthService;
+import com.follower.fizz.util.SendMessageUtil;
 import org.omg.PortableInterceptor.Interceptor;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,15 +19,24 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class SendMessageInterceptor implements HandlerInterceptor {
 
-    @Resource(name = "")
+    @Resource(name = "checkSendMessageAuthService")
     private ICheckSendMessageAuthService iCheckSendMessageAuthService;
 
     private static final String ROOMID_PARAM_STRING = "roomId";
 
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         int roomId = Integer.parseInt(httpServletRequest.getParameter(ROOMID_PARAM_STRING));
-        int userId = Integer.parseInt(httpServletRequest.getSession().getAttribute(SessionParam.USER_ID_KEY);)
-        iCheckSendMessageAuthService.checkSendMessageAuthService(userId, roomId);
+        int userId = Integer.parseInt((String) httpServletRequest.getSession().getAttribute(SessionParam.USER_ID_KEY));
+
+        SendMessageStatus checkResult = iCheckSendMessageAuthService.checkSendMessageAuthService(userId, roomId);
+
+        if (checkResult != null) {
+            SendMessageUtil.sendMessage(httpServletResponse, checkResult);
+            return false;
+        } else {
+            return true;
+        }
+
         return false;
     }
 
